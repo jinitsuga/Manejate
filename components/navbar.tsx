7e935968d6e769106eb64d9592f34e8aeb4dd4ca
agu <iba.agustin@gmail.com>
2023-05-13 22:02:32 -0300

    added navigation back home

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,11 +1,14 @@
 import styles from "./layout.module.css";
+import Link from "next/link";
 import RootLayout from "@/app/layout";
 
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li>Home</li>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
         <li>About</li>
       </ul>
     </nav>
