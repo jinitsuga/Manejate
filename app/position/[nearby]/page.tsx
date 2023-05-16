bafe3ffc5aa8cc55e0621db37b6d68fa2040abf0
agu <iba.agustin@gmail.com>
2023-05-15 22:24:24 -0300

    defined card component to represent open places

--- app/position/[nearby]/page.tsx
+++ app/position/[nearby]/page.tsx
@@ -1,4 +1,5 @@
 import { findPlaces } from "@/helpers/location";
+import Layout from "@/components/layout";
 
 console.log("location pinged");
 export default async function Places({ params }: any) {
@@ -9,29 +10,23 @@ export default async function Places({ params }: any) {
   const places = await findPlaces(lat, long, radius);
 
   const openPlaces = places.results.filter((place: any) => {
-    if (places.results.indexOf(place) < 1) {
-      return false;
-    }
+    // if (places.results.indexOf(place) < 1) {
+    //   return false;
+    // }
     if (!place.opening_hours) {
       return false;
     }
     return place.opening_hours.open_now;
   });
-  console.log(openPlaces);
 
   const locationName: string = places.results[0].name;
+  console.log(openPlaces[2].geometry.location);
 
-  const randomPlace = places.results[3];
-
-  const place = {
-    name: randomPlace.name,
-    isOpen: randomPlace.opening_hours.open_now,
-    type: randomPlace.types,
-  };
-  console.log(place);
   return (
-    <div>
-      <h2>You are in {locationName}</h2>
-    </div>
+    <Layout>
+      <div>
+        <h2>You are in {locationName}</h2>
+      </div>
+    </Layout>
   );
 }
---
+++ components/placeCard.tsx
@@ -0,0 +1,16 @@
+export type Place = {
+  name: string;
+  open: boolean;
+  lat: number;
+  lng: number;
+  types: Array<string>;
+};
+export default function placeCard({ name, open, types }: Place) {
+  return (
+    <div>
+      <h4>{name}</h4>
+      {open ? <span>OPEN</span> : <span>Closed</span>}
+      <span>{types[0]}</span>
+    </div>
+  );
+}
