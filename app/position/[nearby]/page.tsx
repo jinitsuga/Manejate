b04f37da01205874951d52b912f8594a5083dfb3
agu <iba.agustin@gmail.com>
2023-05-15 21:52:22 -0300

    places  are filtered by open hours

--- app/position/[nearby]/page.tsx
+++ app/position/[nearby]/page.tsx
@@ -7,13 +7,31 @@ export default async function Places({ params }: any) {
   const radius = Number(params.nearby.split("~")[2]);
 
   const places = await findPlaces(lat, long, radius);
-  console.log(places);
+
+  const openPlaces = places.results.filter((place: any) => {
+    if (places.results.indexOf(place) < 1) {
+      return false;
+    }
+    if (!place.opening_hours) {
+      return false;
+    }
+    return place.opening_hours.open_now;
+  });
+  console.log(openPlaces);
 
   const locationName: string = places.results[0].name;
 
+  const randomPlace = places.results[3];
+
+  const place = {
+    name: randomPlace.name,
+    isOpen: randomPlace.opening_hours.open_now,
+    type: randomPlace.types,
+  };
+  console.log(place);
   return (
     <div>
-      <h2>You are in {locationName} </h2>
+      <h2>You are in {locationName}</h2>
     </div>
   );
 }
