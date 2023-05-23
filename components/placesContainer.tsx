0575b586a4dddb0d4e185dba4cda160291dcfd8b
agu <iba.agustin@gmail.com>
2023-05-22 23:34:03 -0300

    typed placescontainer, started adding filters to show different places

--- components/placesContainer.tsx
+++ components/placesContainer.tsx
@@ -2,20 +2,48 @@ import { useState } from "react";
 import PlaceCard from "./placeCard";
 import { Place } from "./placeCard";
 
+type Origin = { lat: number; lng: number };
+
 type Places = {
   places: Array<Place>;
+  origin: Origin;
 };
 type PlaceFilter = string;
 
-export const PlacesContainer = ({ places }: Places) => {
-  const [filter, setFilter] = useState<PlaceFilter>("");
+export const PlacesContainer = ({ places, origin }: Places) => {
+  const [filter, setFilter] = useState<PlaceFilter>("all");
 
-  const foodPlaces = places.filter((place) => place.types.includes("food"));
+  //   const foodPlaces = places.filter((place) => place.types.includes("food"));
   const lodgingPlaces = places.filter(
-    (place) =>
+    (place: Place) =>
       place.types.includes("lodging") ||
       place.types.includes("real_estate_agency")
   );
+  const cards = () => {
+    if (filter === "food") {
+      const foodPlaces = places.filter((place: Place) =>
+        place.types.includes("food")
+      );
+      return foodPlaces.map((place: any, id: number) => {
+        const placeLat: number = place.geometry.location.lat;
+        const placeLng: number = place.geometry.location.lng;
+        return (
+          <li>
+            <PlaceCard
+              id={place.place_id}
+              key={id}
+              types={place.types}
+              name={place.name}
+              origin={origin}
+              lat={placeLat}
+              lng={placeLng}
+              open={place.opening_hours.open_now}
+            />
+          </li>
+        );
+      });
+    }
+  };
 
   return <section></section>;
 };
