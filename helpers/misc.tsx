fa2e6d7da12e167b8cc1c547c63dffcf04e4380d
agu <iba.agustin@gmail.com>
2023-05-22 23:43:25 -0300

    declared generic filtering function for placeCards in container

---
+++ helpers/misc.tsx
@@ -0,0 +1,28 @@
+import { Place } from "@/components/placeCard";
+import PlaceCard from "@/components/placeCard";
+
+type Filtered = {
+  places: Array<Place>;
+  origin: { lat: number; lng: number };
+};
+
+const showFilteredCards = ({ places, origin }: Filtered) => {
+  return places.map((place: any, id: number) => {
+    const placeLat: number = place.geometry.location.lat;
+    const placeLng: number = place.geometry.location.lng;
+    return (
+      <li>
+        <PlaceCard
+          id={place.place_id}
+          key={id}
+          types={place.types}
+          name={place.name}
+          origin={origin}
+          lat={placeLat}
+          lng={placeLng}
+          open={place.opening_hours.open_now}
+        />
+      </li>
+    );
+  });
+};
