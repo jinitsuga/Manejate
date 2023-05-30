7fd961e8ba8de8fc8bd061992a6240a886f5016b
agu <iba.agustin@gmail.com>
2023-05-30 09:45:43 -0300

    added about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,39 @@
 import Layout from "@/components/layout";
+import styles from "../page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+import ghIcon from "../icons/gh.png";
 
 export default async function About() {
   return (
     <Layout>
-      {" "}
-      <div>LOL ABOUT</div>
+      <section className={styles.main}>
+        <h3 className=""></h3>
+        <div className={styles.textContainer}>
+          Soy Agu, desarrollador, y este es mi primer proyecto con Nextjs y tal
+          vez le sirva a algún viajero por ahí :)
+        </div>
+        <ul className={styles.btnList}>
+          <li>
+            <Link href={"https://github.com/jinitsuga"}>
+              <Image
+                src={ghIcon}
+                width={30}
+                height={30}
+                alt="github icon"
+              ></Image>
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={styles.regularBtn}
+              href={"https://agu-portfolio.vercel.app/"}
+            >
+              Portfolio
+            </Link>
+          </li>
+        </ul>
+      </section>
     </Layout>
   );
 }
